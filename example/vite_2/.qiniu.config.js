db494da7360f312125581ebca80d508bce6d3fe2
tanghao <tanghao@youloft.com>
2022-08-04 18:47:47 +0800

    ignore 调整

--- example/vite_2/.qiniu.config.js
+++ example/vite_2/.qiniu.config.js
@@ -5,7 +5,6 @@ module.exports = {
 	secretKey: 'SQBkKHUzEfYoHf2V_CcM_uLM9vV1IbRlRzgUoIMa',
 	bucket: 'wnlother',
 	bucketDomain: 'https://qiniu.other.cq-wnl.com/',
-	matchFiles: ['!*.html', '!*.map'], // 七牛过滤不需
 	uploadPath: `/${uploadPath}/`,
 	usePublicPath: true,
 	batch: 10,
