0a70fcd2f6b6324d4d1b22f644d9349c6c1c228f
tanghao <tanghao@youloft.com>
2022-08-04 18:43:27 +0800

    调整ignore

--- example/vite_2/.qiniu.config.js
+++ example/vite_2/.qiniu.config.js
@@ -5,10 +5,11 @@ module.exports = {
 	secretKey: 'SQBkKHUzEfYoHf2V_CcM_uLM9vV1IbRlRzgUoIMa',
 	bucket: 'wnlother',
 	bucketDomain: 'https://qiniu.other.cq-wnl.com/',
-	matchFiles: ['!*.html', '!*.map'],
+	matchFiles: ['!*.html', '!*.map'], // 七牛过滤不需
 	uploadPath: `/${uploadPath}/`,
 	usePublicPath: true,
 	batch: 10,
 	deltaUpdate: true,
 	createCli: 'vue',
+	ignore: ['**/*.html', '**/*.map']
 }
