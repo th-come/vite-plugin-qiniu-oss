6bf3ff8d103d688e7d31bccaa1f6bdf9451c9b80
tanghao <tanghao@youloft.com>
2022-08-04 15:44:27 +0800

    ignore

--- example/vite_2/vite.config.js
+++ example/vite_2/vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vitePluginQiniuOss from '../../index'
 const uploadPath = require('./package.json').name;
 
-
 export default defineConfig({
 	base: `https://qiniu.other.cq-wnl.com/${uploadPath}/`, // same with webpack public path
 	plugins: [vue(), vitePluginQiniuOss()]
