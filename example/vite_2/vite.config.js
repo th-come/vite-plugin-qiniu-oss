27d0d5296b666731b254c5a512c6fcf1f7e2d5d8
tanghao <tanghao@youloft.com>
2022-08-08 10:06:14 +0800

    开启是否上传接口

--- example/vite_2/vite.config.js
+++ example/vite_2/vite.config.js
@@ -5,5 +5,5 @@ const uploadPath = require('./package.json').name;
 
 export default defineConfig({
 	base: `https://qiniu.other.cq-wnl.com/${uploadPath}/`, // same with webpack public path
-	plugins: [vue(), vitePluginQiniuOss()]
+	plugins: [vue(), vitePluginQiniuOss(true)]
 })
